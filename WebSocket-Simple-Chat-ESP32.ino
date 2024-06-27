11c81083c5b11d9b54d3f9338561e48502935902
helloaltop <helloaltop@gmail.com>
2024-06-27 15:25:22 +0800

    Create WebSocket-Simple-Chat-ESP32.ino

---
+++ WebSocket-Simple-Chat-ESP32.ino
@@ -0,0 +1,63 @@
+
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <WebSocketsServer.h>
+#include "index.h"
+
+const char* ssid = "alitopan";     // CHANGE IT
+const char* password = "AliTopan2024";  // CHANGE IT
+
+AsyncWebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);  // WebSocket server on port 81
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t* payload, size_t length) {
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connected from %d.%d.%d.%d\n", num, ip[0], ip[1], ip[2], ip[3]);
+      }
+      break;
+    case WStype_TEXT:
+      Serial.printf("[%u] Received text: %s\n", num, payload);
+      // Send a response back to the client
+      webSocket.sendTXT(num, "Received:  " + String((char*)payload));
+      break;
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Initialize WebSocket server
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+
+  // Serve a basic HTML page with JavaScript to create the WebSocket connection
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+    Serial.println("Web Server: received a web page request");
+    String html = HTML_CONTENT;  // Use the HTML content from the index.h file
+    request->send(200, "text/html", html);
+  });
+
+  server.begin();
+  Serial.print("ESP32 Web Server's IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  webSocket.loop();
+  // TODO: Your code here
+}
